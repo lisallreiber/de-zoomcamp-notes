4552ebdfe51802d936611c70ef9025a0cef2afaf
lisallreiber <lisallreiber@gmail.com>
2023-01-29 23:59:33 +0100

    doc: add inline comments

--- week_1_basics_n_setup/2_docker_sql/ingest-data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest-data.py
@@ -6,12 +6,11 @@
 # !pip install -m sqlalchemy
 # !pip install -m psycopg2
 
-import os
-import argparse
-import pandas as pd
-
-from time import time
-from sqlalchemy import create_engine # to help with named arguments
+import os                              # to run shell commands
+import argparse                        # to parse arguments
+import pandas as pd                    # to read csv files
+from time import time                  # to measure time
+from sqlalchemy import create_engine   # connect to db
 
 
 def main(params):
@@ -25,7 +24,7 @@ def main(params):
     url = params.url
 
     # step01: download data
-    # ----------------------------
+    # ---------------------------------
 
     # the backup files are gzipped, and it's important to keep the correct extension
     # for pandas to be able to open the file
@@ -34,49 +33,50 @@ def main(params):
     else:
         csv_name = 'taxi_data.csv'
 
+    # download the data
     os.system(f"wget {url} -O {csv_name}")
 
     # step02: import data
+    # ---------------------------------
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
     df = next(df_iter)
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-    # step02: connect to postgres
+    # step03: connect to postgres
+    # ---------------------------------
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db_name}')
 
-    # step03: writing the data into the databas in batches
+    # step04: writing the data into the database in batches
+    # ---------------------------------
     # first batch
     df.head(n=0).to_sql(name=tbl_name, con=engine, if_exists='replace')
     df.to_sql(name=tbl_name, con=engine, if_exists='append')
 
     # all other batches
     while True:
-
+        # try to get the next batch
         try:
             t_start = time()
-
             df = next(df_iter)
 
             df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
             df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
             df.to_sql(name=tbl_name, con=engine, if_exists='append')
-
             t_end = time()
 
             print('inserted another chunk, took %.3f seconds' % (t_end - t_start))
-
+        # if there are no more batches, break the loop
         except StopIteration:
             print("Finished ingesting data into the postgres database")
             break
 
 if __name__ == '__main__':
-
     # parse all the arguments
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
-
+    # add arguments
     parser.add_argument('--user', required=True, help='user name for postgres')
     parser.add_argument('--password', required=True, help='password for postgres')
     parser.add_argument('--host', required=True, help='host for postgres')
@@ -89,6 +89,3 @@ if __name__ == '__main__':
 
     # pass the arguments to the main method
     main(args)
-
-
-
