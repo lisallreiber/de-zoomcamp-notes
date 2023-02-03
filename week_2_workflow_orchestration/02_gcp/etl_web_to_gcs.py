02f1fff234a6811856905377f105bdbdec588a32
lisallreiber <lisallreiber@gmail.com>
2023-02-03 14:33:49 +0100

    update: code along 2.2.3 - add tasks

--- week_2_workflow_orchestration/02_gcp/etl_web_to_gcs.py
+++ week_2_workflow_orchestration/02_gcp/etl_web_to_gcs.py
@@ -1,19 +1,29 @@
-from pathlib import Path   # standard libary to deal with file paths
-from pandas import pandas as pd   # popular lib to work with data
-from prefect import flow, task
-from prefect_gcp.cloud_storage import GcsBucket
-from random import randint
+from pathlib import Path                            # standard libary to deal with file paths
+from pandas import pandas as pd                     # popular lib to work with data
+from prefect import flow, task                      # to create a flow and tasks
+from prefect.tasks import task_input_hash           # to create a cache key 
+from datetime import timedelta                      # to set cache expiration
+from prefect_gcp.cloud_storage import GcsBucket     # to upload to GCS
 
-@task(retries=3)
+
+@task(name="fetch data",log_prints=True, retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Fetch data from a URL into Pandas DataFrame"""
-    if randint(0,1) > 0:
-        raise Exception("Random Error")
-        
+
     df = pd.read_csv(dataset_url)
     return df
 
-@flow()
+@task(name="clean data",log_prints=True)
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix dtype issues"""
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+@flow("etl-yellow-taxi")
 def etl_web_to_gcs() -> None:
     """Main ETL Function"""
     color = "yellow"
@@ -23,6 +33,7 @@ def etl_web_to_gcs() -> None:
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
     df = fetch(dataset_url)
+    df_clean = clean(df)
 
 
 if __name__ == "__main__":
