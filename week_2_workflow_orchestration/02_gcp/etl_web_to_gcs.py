0ca0bacc3bf729c8698a46cc1af1cdfb65a44735
lisallreiber <lisallreiber@gmail.com>
2023-02-03 13:56:52 +0100

    add random integer example for demonstrating the retries functionality in prefect tasks

--- week_2_workflow_orchestration/02_gcp/etl_web_to_gcs.py
+++ week_2_workflow_orchestration/02_gcp/etl_web_to_gcs.py
@@ -2,10 +2,14 @@ from pathlib import Path   # standard libary to deal with file paths
 from pandas import pandas as pd   # popular lib to work with data
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
+from random import randint
 
 @task(retries=3)
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Fetch data from a URL into Pandas DataFrame"""
+    if randint(0,1) > 0:
+        raise Exception("Random Error")
+        
     df = pd.read_csv(dataset_url)
     return df
 
